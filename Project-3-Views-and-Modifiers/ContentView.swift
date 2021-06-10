49b4b31c6125361161b07cb39e1cc2bfe222256b
Baris <karalarbaris@gmail.com>
2021-06-10 07:12:41 +0200

    Initial Commit

---
+++ Project-3-Views-and-Modifiers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project-3-Views-and-Modifiers
+//
+//  Created by Baris Karalar on 10.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project-3-Views-and-Modifiers/Project_3_Views_and_ModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_3_Views_and_ModifiersApp.swift
+//  Project-3-Views-and-Modifiers
+//
+//  Created by Baris Karalar on 10.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct Project_3_Views_and_ModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
